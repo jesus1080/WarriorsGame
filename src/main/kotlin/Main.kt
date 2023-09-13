6794d5299f6a2f57d24ec5a36992de1d0ed9b9f9
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2023-09-12 22:13:05 -0500

    add logic in main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
+import edu.unicauca.aplimovil.model.Guerrero
+import edu.unicauca.aplimovil.model.Hechicero
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    val hechicero = Hechicero("Merlyn")
+    val guerrero = Guerrero("Arturo")
+    val guerrerodos = Guerrero("Aragon")
+
+    println(hechicero.presentarse())
+    println(guerrero.presentarse())
+
+    hechicero.mana = 2
+    guerrero.fuerza = 4
+
+    println(hechicero.mana)
+    println(guerrero.fuerza)
+
+    hechicero.hechizar(guerrero)
+    guerrero.atacar(guerrerodos)
+
+    println(guerrerodos.vida)
+    hechicero.sanar(guerrerodos)
+
+    println(guerrerodos.vida)
+
+    
 }
